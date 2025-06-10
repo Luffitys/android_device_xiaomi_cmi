b1bcd172dadd1c21e20261fa956a693929467c48
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-10 18:39:13 +0200

    cmi: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,6 @@ TARGET_SCREEN_DENSITY := 440
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_cmi
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_cmi
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/cmi.config
